7e3b81633efdabc3e70cccd883c1d9f82700253c
anushbareyan <anushbareyan@gmail.com>
2025-05-02 20:31:26 +0400

    feedback

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -77,24 +77,24 @@ const Feedback = () => {
           {error && <Alert variant="danger">{error}</Alert>}
 
           <Form.Group className="mb-3">
-            <Form.Label>Name</Form.Label>
+            <Form.Label>Name(not required)</Form.Label>
             <Form.Control
               type="text"
               placeholder="Your name"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              required
+              // required
             />
           </Form.Group>
 
           <Form.Group className="mb-3">
-            <Form.Label>Email</Form.Label>
+            <Form.Label>Email(not required)</Form.Label>
             <Form.Control
               type="email"
               placeholder="Your email address"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              required
+              // required
             />
           </Form.Group>
 
